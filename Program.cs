a722036ba9b99632e7b67a441df81a1338b3d254
Erkhembayar Enkhtaivan <erkhemee0908@gmail.com>
2023-12-28 13:56:52 +0800

    v 1.0.1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 namespace constructor.sqr;
 internal class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         if (args.Length != 1)
         {
